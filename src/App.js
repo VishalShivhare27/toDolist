39202fc44c91a801a8a59ad8ce8f9f286d880de1
Vishal Shivhare <vishalshivhare27021998@gmail.com>
2020-06-16 19:54:54 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import ToDoList from "./ToDoList";
 
 function App() {
+const [inputList,setInputList] = useState("");
+const [Items, setItems] = useState([]);
+
+const itemEvent = (event) => {
+  setInputList(event.target.value);
+};
+
+const listOfItems = ()=> {
+  setItems((oldItems) => {
+    return [...oldItems, inputList];
+  });
+  setInputList("");
+};
+const deleteItems=(id) =>{
+setItems((oldItems)=> {
+  return oldItems.filter((arrElem,index)=> {
+      return index !==id;
+  });
+});
+};
+
   return (
     <>
     <div className="main_div">
@@ -9,11 +31,23 @@ function App() {
         <br />
         <h1> ToDo list</h1>
         <br />
-        <input type = "text" placeholder= "Add a Task!"/>
-        <button> + </button>
+        <input type = "text" placeholder= "Add a Task!" value={inputList} onChange={itemEvent}/>
+        <button onClick={listOfItems}> + </button>
 
         <ol>
-          <li> Buy Apples</li>
+ {/* <li> {inputList}</li> */}
+
+    {  Items.map(( itemval,index) => {
+         return <ToDoList 
+        key={index}
+        id={index}
+        text = {itemval}
+        onSelect= {deleteItems}
+          />
+      })}
+
+      
+
         </ol>
       </div>
     </div>
